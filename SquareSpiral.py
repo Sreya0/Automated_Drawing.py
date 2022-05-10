9c5433791c3090847bd6cf9b2893bad65619ce96
Sreya0 <102013015+Sreya0@users.noreply.github.com>
2022-05-11 00:42:07 +0530

    Update SquareSpiral.py

--- SquareSpiral.py
+++ SquareSpiral.py
@@ -3,10 +3,10 @@ import time
 time.sleep(3)
 distance = 200
 while distance > 0:
-    pyautogui.dragRel(distance, 0, button="left")
+    pyautogui.dragRelative(distance, 0, button="left")
     distance = distance - 10
-    pyautogui.dragRel(0, distance, button="left")
-    pyautogui.dragRel(-distance, 0, button="left")
+    pyautogui.dragRelative(0, distance, button="left")
+    pyautogui.dragRelative(-distance, 0, button="left")
     distance = distance - 10
-    pyautogui.dragRel(0, -distance, button="left")
+    pyautogui.dragRelative(0, -distance, button="left")
    
